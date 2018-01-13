5f8f552fa71aac615993aca3dd2fb57765cdab7c
Kimia Kalantari <kimiakalantari1356@gmail.com>
2018-01-13 00:00:51 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+// First Function
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for(var i = 0; i < musicians.length; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+        }
+        return array;
+      }
+
+// Second Function
+function johnLennonFacts(facts){
+  var NewArray = [];
+  var i = '';
+  while (i < facts.length){
+    NewArray.push(`${facts}!!!`)
+  }
+
+    return NewArray;
+}
+
+// Third Function
+function iLoveTheBeatles(number) {
+  var NewArray2 = []
+  do {
+    NewArray2.push("I love the Beatles!")
+    number++
+  } while (number < 15)
+    return NewArray2;
+}
